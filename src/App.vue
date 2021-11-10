8d447f2eaaf9d987aaeac0cc0614a656ce53e13f
EvilYep <guillaume.moneret@gmail.com>
2021-11-10 08:10:04 +0100

    :recycle: refactored

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,32 @@
 <template>
-    <div id="app">
-        @{{ user.username }} - {{ fullName }}
-        <strong>followers: </strong> {{ followersCount }}
-        <button @click="followUser">
-            Follow
-        </button>
+    <div id="main">
+        <UserProfile/>
     </div>
 </template>
 
 <script>
+import UserProfile from './components/UserProfile';
+
 export default {
     name: "App",
 
-    data() {
-        return {
-            followersCount: 0,
-            user: {
-                id: 1,
-                username: '_Yep',
-                firstName: 'Yep',
-                lastName: 'Derp',
-                email: 'yep@derp.com',
-                admin: true
-            }
-        }
-    },
-
-    watch: {
-        followersCount(newCount, oldCount) {
-            if(oldCount < newCount) {
-                console.log(`${this.user.username} has gained a follower !`);
-            }
-        }
-    },
-
-    computed: {
-        fullName() {
-            return `${this.user.firstName} ${this.user.lastName}`
-        }
-    },
-
-    methods: {
-        followUser() {
-            this.followersCount++;
-        }
-    },
-
-    mounted() {
-        this.followUser();
+    components: {
+        UserProfile
     }
 };
 </script>
 
 <style>
-#app {
+body {
+    margin: 0;
+}
+
+#main {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
-    display: flex;
-    flex-direction: column;
-    text-align: center;
+    min-height: 100vh;
+    background-color: #f3f5fa;
 }
 </style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="user-profile">
+        <div class="user-profile_user-panel">
+            <h1 class="user-profile_username">@{{ user.username }}</h1>
+            <div class="user-profile_followers-count">
+                <strong>Followers :</strong> {{ followersCount }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'UserProfile',
+
+    data() {
+        return {
+            followersCount: 0,
+            user: {
+                id: 1,
+                username: '_Yep',
+                firstName: 'Yep',
+                lastName: 'Derp',
+                email: 'yep@derp.com',
+                admin: true
+            }
+        }
+    },
+
+    watch: {
+        followersCount(newCount, oldCount) {
+            if(oldCount < newCount) {
+                console.log(`${this.user.username} has gained a follower !`);
+            }
+        }
+    },
+
+    computed: {
+        fullName() {
+            return `${this.user.firstName} ${this.user.lastName}`
+        }
+    },
+
+    methods: {
+        followUser() {
+            this.followersCount++;
+        }
+    },
+
+    mounted() {
+        this.followUser();
+    }
+}
+</script>
+
+<style>
+.user-profile {
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    width: 100%;
+    padding: 50px 5%;
+}
+
+.user-profile_user-panel {
+    display: flex;
+    flex-direction: column;
+    margin-right: 50px;
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid #dfe3e8;
+}
+
+h1.user-profile_username {
+    margin: 0;
+}
+</style>
